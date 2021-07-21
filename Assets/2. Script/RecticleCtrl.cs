5b917af9e554f0a3e66da2788de6a84b6e96d5f8
sjw03115 <37091470+sjw03115@users.noreply.github.com>
2021-07-21 14:57:03 +0900

    Add files via upload

---
+++ Assets/2. Script/RecticleCtrl.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecticleCtrl : MonoBehaviour
+{
+    public Image recticle;
+    public GameObject TrevalA;
+    public GameObject TrevalB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(Camera.main.transform.position,
+        Camera.main. transform. forward) ;
+
+        RaycastHit hit; //레이저랑 부딪히는 오브젝트가 들어가는곳
+       
+        //레이저에 부딪치는게 있으면~
+        if(Physics.Raycast(ray,out hit, 20f))
+        {
+            recticle.fillAmount += Time.deltaTime;
+
+            if( recticle.fillAmount == 1)
+            {
+                TrevalA.SetActive(false);
+                TrevalB.SetActive(true);
+            }
+        }
+
+        else //아무것도 부딪치는게 없다.
+        {
+            recticle.fillAmount = 0;
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 1f);
+    }
+}
